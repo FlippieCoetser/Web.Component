1a954f7005ff14f1a81b56c83148709718d503f2
Flippie.Coetser <flippie.coetser@gmail.com>
2022-07-11 04:07:20 -0700

    Add Enum to clean scripts

--- tasks/clean.lib.mjs
+++ tasks/clean.lib.mjs
@@ -1,3 +1,3 @@
 #! /usr/bin/env node
 import del from './del.mjs'
-del(['./lib/*.js', './lib/*.d.ts']);
+del(['./lib/*.js', './lib/*.d.ts', './lib/enums/*.d.ts', './lib/enums/*.js']);
--- tasks/clean.test.mjs
+++ tasks/clean.test.mjs
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 import del from './del.mjs'
-del(['./src/*.js', './src/*.map']);
+del(['./src/*.js', './src/*.map','./src/enums/*.js','./src/enums/*.map']);
 del(['./test/*.js', './test/*.map']);
