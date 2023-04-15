52151d2595d36c0036d775380ccaefe80be112f6
Flippie.Coetser <flippie.coetser@gmail.com>
2023-04-15 10:15:56 -0700

    COMPONENTS: Add New Component Tags

--- src/enums/enum.tag.ts
+++ src/enums/enum.tag.ts
@@ -18,5 +18,8 @@ export enum Tag {
     Maximize = 'button-maximize',
     Minimize = 'button-minimize',
     Close = 'button-close',
-    Header = 'bar-header'
+    Header = 'bar-header',
+    Hamburger = 'button-hamburger',
+    Contents = 'button-contents',
+    Fullscreen = 'button-fullscreen'
 }
